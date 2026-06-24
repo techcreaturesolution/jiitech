a239d913767f905cb3479367aee0e6ca5e0bbf1d
LekhrajMahajan <lekhrajmahajan506@gmail.com>
2026-06-24 14:55:21 +0530

    config file update

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000" || "https://jiitech.onrender.com",
+        target: "https://jiitech.onrender.com" || "http://localhost:5000",
         changeOrigin: true,
       },
     },
